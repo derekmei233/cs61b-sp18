9a2cc7953d45e10c589be7ddf7fa5f8221679a79
derek mei <derekmei233@gmail.com>
2021-08-20 15:54:08 -0400

    update hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,90 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
+import java.net.CookieHandler;
 
+public class Percolation {
+    private final int units;
+    private int numOfOpen;
+    private WeightedQuickUnionUF uf;
+    private int[] sites;
+
+    public Percolation(int N) throws IllegalArgumentException{
+        // create N-N grid, with all sites initially blocked
+        if (N <= 0) {
+            throw new IllegalArgumentException("N should greater than 0");
+        }
+        units = N;
+        numOfOpen = 0;
+        uf = new WeightedQuickUnionUF(units * units);
+        sites = new int[units * units];
+    }
+
+    private void checkIndex(int row, int col) throws IndexOutOfBoundsException {
+        if (row < 0 || row >= units || col < 0 || col >= units) {
+            throw new IndexOutOfBoundsException("invalid (row, col) combination");
+        }
+    }
+
+    private int cordToIndex(int x, int y) {
+        return x * units + y;
+    }
+
+    private void neighboring(int row, int col) {
+        if (row - 1 >= 0 && sites[cordToIndex(row - 1, col)] == 1) {
+            uf.union(cordToIndex(row - 1, col), cordToIndex(row, col));
+        }
+        if (row + 1 <= units - 1 && sites[cordToIndex(row + 1, col)] == 1) {
+            uf.union(cordToIndex(row + 1, col), cordToIndex(row, col));
+        }
+        if (col - 1 >= 0 && sites[cordToIndex(row, col - 1)] == 1) {
+            uf.union(cordToIndex(row, col - 1), cordToIndex(row, col));
+        }
+        if (col + 1 <= units - 1 && sites[cordToIndex(row, col + 1)] == 1) {
+            uf.union(cordToIndex(row, col + 1), cordToIndex(row ,col));
+        }
+    }
+
+    public void open(int row, int col) {
+        // open the site (row, col) if it is not open already
+        checkIndex(row, col);
+        sites[cordToIndex(row, col)] = 1;
+        numOfOpen += 1;
+        neighboring(row, col);
+    }
+    public boolean isOpen(int row, int col) {
+        // is the site (row, col) open?
+        checkIndex(row, col);
+        return sites[cordToIndex(row, col)] == 1;
+    }
+    public boolean isFull(int row, int col) {
+        // is the site(row, col) full?
+        checkIndex(row, col);
+        if (sites[cordToIndex(row, col)] == 0) {
+            return false;
+        }
+        int pos = uf.find(cordToIndex(row, col));
+        for (int i = 0; i < units; i ++) {
+            if (uf.find(i) == pos) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public int numberOfOpenSites() {
+        // number of open sites
+        return numOfOpen;
+    }
+    public boolean percolates() {
+        // does the system percolate?
+        for (int i = 0; i < units; i++) {
+            if (isFull(units - 1, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        // unittest here
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,54 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdStats;
 
+import static edu.princeton.cs.algs4.StdRandom.*;
+
+
+public class PercolationStats {
+    private final int units;
+    private final int times;
+    private PercolationFactory pF;
+    private int[] mem;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        // perform T independent experiments on an N-by-N grid
+        units = N;
+        times = T;
+        pF = pf;
+        mem = new int[T];
+        Percolation percoSys;
+        int count;
+        for (int i = 0; i < T; i++) {
+            int[] que = creatShuffledQueue();
+            percoSys = pf.make(N);
+            count = 0;
+            while (!percoSys.percolates()) {
+                percoSys.open(que[count] / units, que[count] % units);
+                count += 1;
+            }
+            mem[i] = count;
+        }
+    }
+    private int[] creatShuffledQueue() {
+        int[] que = new int[units * units];
+        shuffle(que);
+        return que;
+    }
+    public double mean() {
+        // sample mean of percolation threshold
+        return StdStats.mean(mem);
+    }
+    public double stddev() {
+        // sample standard deviation of percolation threshold
+        return StdStats.stddev(mem);
+    }
+    public double confidenceLow() {
+        // low endpoint of 95% confidence interval
+        return mean() - 1.96 * stddev() / Math.sqrt(mem.length);
+    }
+    public double confidenceHigh() {
+        // high endpoint of 95% confidence interval
+        return mean() + 1.96 * stddev() / Math.sqrt(mem.length);
+    }
 }
