ed48c551423e3be07bfc6d10a430e4306b3ee81f
derek mei <derekmei233@gmail.com>
2021-08-26 10:50:42 -0400

    fix testcomplexoomage

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -1,5 +1,6 @@
 package hw3.hash;
 
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -32,13 +33,26 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
+    private static ComplexOomage randomComplexOomageAttack() {
+        int N = StdRandom.uniform(8, 10);
+        ArrayList<Integer> params = new ArrayList<>(N);
+        for (int i = 0; i < N - 4; i += 1) {
+            params.add(StdRandom.uniform(0, 255));
+        }
+        params.add(0);
+        params.add(0);
+        params.add(0);
+        params.add(0);
+        params.add(0);
+        return new ComplexOomage(params);
+    }
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
         int N = 10000;
 
         for (int i = 0; i < N; i += 1) {
-            deadlyList.add(ComplexOomage.randomComplexOomageAttack());
+            deadlyList.add(TestComplexOomage.randomComplexOomageAttack());
         }
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
     }
