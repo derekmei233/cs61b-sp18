e2c721db27472a231976b6311b631e2369bbf59d
derek mei <derekmei233@gmail.com>
2021-08-27 21:25:38 -0400

    update lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
+import java.lang.annotation.Target;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +13,51 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new Queue<>();
+        marked[s] = true;
+        queue.enqueue(s);
+        int temp;
+        while(!queue.isEmpty()) {
+            temp = queue.dequeue();
+            if (temp == t) {
+                targetFound = true;
+            }
+            if (targetFound) {
+                return;
+            }
+            for (int step: maze.adj(temp)) {
+                if (!marked[step]) {
+                    distTo[step] = distTo[temp] + 1;
+                    edgeTo[step] = temp;
+                    marked[step] = true;
+                    queue.enqueue(step);
+                }
+            }
+            announce();
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs();
     }
 }
 
