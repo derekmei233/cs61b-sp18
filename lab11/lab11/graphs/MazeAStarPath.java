e7c0534650089ef7a60febae4e765c7a248deb22
derek mei <derekmei233@gmail.com>
2021-08-30 14:54:27 -0400

    finish lab11 astar

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -8,6 +11,7 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    private PriorityQueue<Integer> minPQ;
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,22 +20,50 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        minPQ = new PriorityQueue<Integer>(100, new MyComparator());
+    }
+    private class MyComparator implements Comparator<Integer> {
+        public int compare(Integer i, Integer j) {
+            return Integer.compare(h(i), h(j));
+        }
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int distance;
+        distance = distTo[v] + Math.abs(maze.toX(v) - maze.toX(t))
+                + Math.abs(maze.toY(v) - maze.toY(t));
+        return distance;
     }
 
     /** Finds vertex estimated to be closest to target. */
     private int findMinimumUnmarked() {
+        if (!minPQ.isEmpty()) {
+            return minPQ.peek();
+        }
         return -1;
         /* You do not have to use this method. */
     }
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        minPQ.add(s);
+        marked[s] = true;
+        while (!minPQ.isEmpty()) {
+            int cur = minPQ.remove();
+            for (int i: maze.adj(cur)) {
+                if (!marked[i]) {
+                    minPQ.add(i);
+                    marked[i] = true;
+                    distTo[i] = distTo[cur] + 1;
+                    edgeTo[i] = cur;
+                    announce();
+                    if (marked[t]) {
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     @Override
